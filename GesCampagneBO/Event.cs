76214c084bbf7524f84f4a14c0e685d4a7a6dcd4
romain barron <rom-tennis@hotmail.fr>
2015-11-10 14:51:30 +0100

    modif bo

--- GesCampagneBO/Event.cs
+++ GesCampagneBO/Event.cs
@@ -5,7 +5,61 @@ using System.Text;
 
 namespace GesCampagneBO
 {
-    class Event
+    public class Evenement
     {
+        private int id;
+        private string theme;
+        private DateTime dateDebut;
+        private DateTime dateFin;
+        //private Ville laVille;
+        //private Campagne laCampagne;
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Theme
+        {
+            get { return theme; }
+            set { theme = value; }
+        }
+
+
+        public DateTime DateDebut
+        {
+            get { return dateDebut; }
+            set { dateDebut = value; }
+        }
+
+
+        public DateTime DateFin
+        {
+            get { return dateFin; }
+            set { dateFin = value; }
+        }
+
+        public Evenement(int id, string theme, DateTime dateDebut, DateTime dateFin)//Ville laVille,Campagne laCampagne)
+        {
+            this.id = id;
+            this.theme = theme;
+            this.dateDebut = dateDebut;
+            this.dateFin = dateFin;
+            //this.laVille=laVille;
+            //this.laCampagne=laCampagne;
+        }
+
+        public Evenement(string theme, DateTime dateDebut, DateTime dateFin)//Ville laVille,Campagne laCampagne)
+        {
+            this.theme = theme;
+            this.dateDebut = dateDebut;
+            this.dateFin = dateFin;
+            //this.laVille=laVille;
+            //this.laCampagne=laCampagne;
+        }
+
+
     }
 }
--- GesCampagneBO/Ville.cs
+++ GesCampagneBO/Ville.cs
@@ -7,5 +7,35 @@ namespace GesCampagneBO
 {
     class Ville
     {
+        private string libelle;
+        private int numInsee;
+        private string cp;
+
+        public int NumInsee
+        {
+            get { return numInsee; }
+            set { numInsee = value; }
+        }
+
+
+        public string Libelle
+        {
+            get { return libelle; }
+            set { libelle = value; }
+        }
+
+
+        public string Cp
+        {
+            get { return cp; }
+            set { cp = value; }
+        }
+
+        public Ville(int numInsee, string libelle, string cp)
+        {
+            this.numInsee = numInsee;
+            this.libelle = libelle;
+            this.cp = cp;
+        }
     }
 }
