9382bc88d9b7eb09c0d05b2928657a92e97e527e
yoann betourne <yoann.betourne@live.fr>
2015-11-10 15:20:21 +0100

    modification de frmAjoutArtistre , ArtisteDAO et ajout d'une reference à GesCampagneDAl

--- ApplicationGestionCampagne/FrmAjoutArtiste.Designer.cs
+++ ApplicationGestionCampagne/FrmAjoutArtiste.Designer.cs
@@ -28,19 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAjoutArtiste = new System.Windows.Forms.Button();
+            this.lblTitreAjoutArtiste = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblArtiste = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(157, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 17);
+            this.listBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(157, 126);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // btnAjoutArtiste
+            // 
+            this.btnAjoutArtiste.Location = new System.Drawing.Point(157, 214);
+            this.btnAjoutArtiste.Name = "btnAjoutArtiste";
+            this.btnAjoutArtiste.Size = new System.Drawing.Size(100, 32);
+            this.btnAjoutArtiste.TabIndex = 12;
+            this.btnAjoutArtiste.Text = "Ajouter un artiste";
+            this.btnAjoutArtiste.UseVisualStyleBackColor = true;
+            // 
+            // lblTitreAjoutArtiste
+            // 
+            this.lblTitreAjoutArtiste.AutoSize = true;
+            this.lblTitreAjoutArtiste.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitreAjoutArtiste.Location = new System.Drawing.Point(106, 34);
+            this.lblTitreAjoutArtiste.Name = "lblTitreAjoutArtiste";
+            this.lblTitreAjoutArtiste.Size = new System.Drawing.Size(171, 24);
+            this.lblTitreAjoutArtiste.TabIndex = 11;
+            this.lblTitreAjoutArtiste.Text = "Ajouter un Artiste";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Courant de l\'artiste :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Site web :";
+            // 
+            // lblArtiste
+            // 
+            this.lblArtiste.AutoSize = true;
+            this.lblArtiste.Location = new System.Drawing.Point(41, 94);
+            this.lblArtiste.Name = "lblArtiste";
+            this.lblArtiste.Size = new System.Drawing.Size(85, 13);
+            this.lblArtiste.TabIndex = 8;
+            this.lblArtiste.Text = "Nom de l\'artiste :";
             // 
             // FrmAjoutArtiste
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(403, 313);
+            this.ClientSize = new System.Drawing.Size(325, 297);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAjoutArtiste);
+            this.Controls.Add(this.lblTitreAjoutArtiste);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblArtiste);
             this.Name = "FrmAjoutArtiste";
             this.Text = "FrmAjoutArtiste";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAjoutArtiste;
+        private System.Windows.Forms.Label lblTitreAjoutArtiste;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblArtiste;
     }
 }
