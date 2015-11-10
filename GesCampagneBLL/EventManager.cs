11ee15ef9dfbab7f312287f75a0f7606f2316744
romain barron <rom-tennis@hotmail.fr>
2015-11-10 15:02:02 +0100

    m

--- GesCampagneBLL/EventManager.cs
+++ GesCampagneBLL/EventManager.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GesCampagneDAL;
+
 
 namespace GesCampagneBLL
 {
     class EventManager
     {
+
+      
     }
 }
