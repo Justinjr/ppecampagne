b339790aae6a2c4286f27c4d1bcf70d7f73b04ea
Margaux Ozon <1margaux.ozon6@gmail.com>
2015-11-10 16:07:47 +0100

    ajout FrmAjoutMessage (design)

---
+++ ApplicationGestionCampagne/FrmAjoutMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ApplicationGestionCampagne
+{
+    public partial class FrmAjoutMessage : Form
+    {
+        public FrmAjoutMessage()
+        {
+            InitializeComponent();
+        }
+    }
+}
