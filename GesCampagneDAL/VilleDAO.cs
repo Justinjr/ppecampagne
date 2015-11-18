8e044e3715be3ec31c26c2c485a0bd6be0f5d1a1
romain barron <rom-tennis@hotmail.fr>
2015-11-18 14:44:32 +0100

    c

--- GesCampagneDAL/VilleDAO.cs
+++ GesCampagneDAL/VilleDAO.cs
@@ -2,12 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GesCampagneBO;
+using System.Configuration;
+using GesCampagneDAL;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace GesCampagneDAL
 {
     public class VilleDAO
     {
+        private static VilleDAO uneInstanceVilleDAO;
+
+        public static VilleDAO GetInstanceDAOVille()
+        {
+            if (uneInstanceVilleDAO == null)
+
+            {
+                uneInstanceVilleDAO = new VilleDAO();
+            }
+            return uneInstanceVilleDAO;
+        }
+
+        public List<Ville> GetVilles()
+        {
+
+
+            //on récup l'objet responsable de la connexion a la base
+            SqlConnection cnx = AccesBD.GetInstance().GetSqlConnexion();
+
+            //on creer la collection lesClients de type list<Client> qui va contenir les clients
+            List<Ville> lesClients = new List<Ville>();
+
+            //crer objet de type sqlCommand
+            SqlCommand maCommand = new SqlCommand();
+
+            //execute la requete
+
+            maCommand.CommandType = CommandType.StoredProcedure;
+            maCommand.CommandText = "GetVille";
+            // maCommand.CommandText = "select nom,prenom  from Client ";
+
+            maCommand.Connection = cnx;
+            SqlDataReader monReader = maCommand.ExecuteReader();
+            while (monReader.Read())
+            {
+                 int numInsee = (int)monReader["numInsee"];
+                 string libelle = (string)monReader["libelle"];
+                 string cp = (string)monReader["cp"];
+
+                lesClients.Add(new Ville(numInsee,libelle, cp));
+
+            }
+
+
+            AccesBD.GetInstance().CloseConnection();
+            return lesClients;
+        }
+
 
     }
-    
+
 }
