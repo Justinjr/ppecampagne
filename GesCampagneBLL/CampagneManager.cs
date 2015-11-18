9f4d665bdee74dcfdfd32a9de886bcba7a8057dc
justin dumas <justin.dumas.jr@hotmail.com>
2015-11-18 15:10:06 +0100

    .

--- GesCampagneBLL/CampagneManager.cs
+++ GesCampagneBLL/CampagneManager.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using GesCampagneBO;
+using GesCampagneDAL;
 
 namespace GesCampagneBLL
 {
     public class CampagneManager
     {
+        private static CampagneManager uneInstanceCampagneManager;
+
+        public static CampagneManager GetInstance()
+        {
+            if (uneInstanceCampagneManager == null)
+            {
+                uneInstanceCampagneManager = new CampagneManager();
+            }
+            return uneInstanceCampagneManager;
+        }
+
+        public void SetChaineConnexion(ConnectionStringSettings connSetting)
+        {
+            string laChaine = connSetting.ConnectionString;
+            AccesBD.GetInstance().SetChaine(laChaine);
+        }
+
+
+        public int CreerCampagne(string unIntitule, string unObjectif, DateTime uneDateDebut, DateTime uneDateFin, int unIdPublic, int unIdEmploye)
+        {
+            Campagne laCampagne;
+            laCampagne = new Campagne( unIntitule, unObjectif, uneDateDebut, uneDateFin, unIdPublic, unIdEmploye);
+            return CampagneDAO.GetInstanceDAOCampagne().AjoutCampagne(laCampagne);
+        }
+
+
+
+
     }
 }
