b98d0306d95da37466a4dd46680df64ff0bbcd7f
yoann betourne <yoann.betourne@live.fr>
2015-11-18 16:33:58 +0100

    ajout de la class courantDAO , ajout de la class CourantManager

---
+++ GesCampagneBLL/CourantManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+using GesCampagneDAL;
+using GesCampagneBO;
+
+namespace GesCampagneBLL
+{
+    public class CourantManager
+    {
+        private static CourantManager uneInstanceCourantManag;
+
+        public static CourantManager GetInstance()
+        {
+            if(uneInstanceCourantManag == null)
+            {
+                uneInstanceCourantManag = new CourantManager();
+            }
+            return uneInstanceCourantManag;
+        }
+        //cette methode permet de fournir la chaine de connexion à la couche DAl
+        public void SetChaineConnexion(ConnectionStringSettings connSetting)
+        {
+            string laChaine = connSetting.ConnectionString;
+            AccesBD.GetInstance().SetChaine(laChaine);
+        }
+        //appel de la couche DAL pour récupéré une collection de clients
+        public List<Courant> GetCourants()
+        {
+            //ici , on peut appliquer des règles métier
+            return CourantDAO.GetInstanceDAOCourant().GetCourants();
+        }
+    }
+}
---
+++ GesCampagneDAL/CourantDAO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GesCampagneBO;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GesCampagneDAL
+{
+    public class CourantDAO
+    {
+        private static CourantDAO uneInstanceCourantDAO;
+
+        //cette methode s'appelle un singleton
+        //elle crée un objet instance de la classeDAO s'il n'existe pas 
+        //puis retourne la reference à cet objet
+        public static CourantDAO GetInstanceDAOCourant()
+        {
+            if (uneInstanceCourantDAO == null)
+            {
+                uneInstanceCourantDAO = new CourantDAO();
+            }
+            return uneInstanceCourantDAO;
+        }
+
+        //la méthode getCourant retourne une collection contenant les lesCourants
+        //existant dans la table Courant
+        public List<Courant> GetCourants()
+        {
+            int idLu;
+            string libelleLu;
+            SqlDataReader monLecteur;
+
+            //on recupere l'objet responsable de la connexion à la base
+            SqlConnection cnx = AccesBD.GetInstance().GetSqlConnexion();
+
+            //on crée la collection lesCourants de type List<Courant> qui va contenir les courants
+            List<Courant> lesCourants = new List<Courant>();
+
+            //on crée l'objet de type SqlCommand qui va contenir la requete SQL
+            //permettant d'obtenir toutes les caractéristiques de tous les courants
+            SqlCommand maCommand = new SqlCommand();
+            maCommand.Connection = cnx;
+            maCommand.CommandType = CommandType.StoredProcedure;
+            maCommand.CommandText = "spSelectCourant";
+            //command.CommandText = "select id, libelle from courant";
+
+            //on execute la requete
+            monLecteur = maCommand.ExecuteReader();
+
+            //pour chaque enregistrement retourné on crée un objet instance de client
+            //que l'on ajoute dans la collection lesCourants
+            while (monLecteur.Read())
+            {
+                //on récupère l'id et le libelle du courant
+                idLu = (int)monLecteur["id"];
+                libelleLu = (string)monLecteur["libelle"];
+
+                Courant unCourant = new Courant(idLu, libelleLu);
+                lesCourants.Add(unCourant);
+            }
+            AccesBD.GetInstance().CloseConnection();
+            return lesCourants;
+        }
+    }
+}
